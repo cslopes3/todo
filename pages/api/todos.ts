9e1080fb6314cbef48036427baa2057654b932b8
cslopes3 <cristianslopes@gmail.com>
2023-07-03 12:08:35 -0300

    feat :sparkles: created todo app

---
+++ pages/api/todos.ts
@@ -0,0 +1,21 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { todoController } from "@server/controller/todo";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    todoController.get(req, res);
+    return;
+  }
+
+  if (req.method === "POST") {
+    todoController.create(req, res);
+    return;
+  }
+
+  res.status(405).json({
+    message: "Method not allowed",
+    error: {
+      message: "Method not allowed",
+    },
+  });
+}
---
+++ src/test/todo.test.ts
@@ -0,0 +1,33 @@
+import handler from "@api/todos";
+import { createMocks, RequestMethod } from "node-mocks-http";
+import { NextApiRequest, NextApiResponse } from "next";
+
+describe("/api/todos API Endpoint", () => {
+  function mockRequestResponse(method: RequestMethod = "GET") {
+    const { req, res }: { req: NextApiRequest; res: NextApiResponse } =
+      createMocks({ method });
+    req.headers = {
+      "Content-Type": "application/json",
+    };
+    return { req, res };
+  }
+
+  it("GET: should return a successful response", async () => {
+    const { req, res } = mockRequestResponse();
+
+    req.query = { page: "2" };
+    await handler(req, res);
+
+    expect(res.statusCode).toBe(200);
+  });
+
+  // it("create a new todo", async () => {
+  //   const { req, res } = mockRequestResponse("POST");
+
+  //   req.body = { content: "Test todo" };
+
+  //   await handler(req, res);
+
+  //   expect(res.statusCode).toBe(201);
+  // });
+});
