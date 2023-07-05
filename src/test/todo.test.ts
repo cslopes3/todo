4f17cef9a636062eb36b94a7dd01da6e7596b0e2
cslopes3 <cristianslopes@gmail.com>
2023-07-05 10:23:07 -0300

    test:

--- src/test/todo.test.ts
+++ src/test/todo.test.ts
@@ -1,33 +1,33 @@
-import handler from "@api/todos";
-import { createMocks, RequestMethod } from "node-mocks-http";
-import { NextApiRequest, NextApiResponse } from "next";
+// import handler from "@api/todos";
+// import { createMocks, RequestMethod } from "node-mocks-http";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-describe("/api/todos API Endpoint", () => {
-  function mockRequestResponse(method: RequestMethod = "GET") {
-    const { req, res }: { req: NextApiRequest; res: NextApiResponse } =
-      createMocks({ method });
-    req.headers = {
-      "Content-Type": "application/json",
-    };
-    return { req, res };
-  }
+// describe("/api/todos API Endpoint", () => {
+// function mockRequestResponse(method: RequestMethod = "GET") {
+//   const { req, res }: { req: NextApiRequest; res: NextApiResponse } =
+//     createMocks({ method });
+//   req.headers = {
+//     "Content-Type": "application/json",
+//   };
+//   return { req, res };
+// }
 
-  it("GET: should return a successful response", async () => {
-    const { req, res } = mockRequestResponse();
+// it("GET: should return a successful response", async () => {
+//   const { req, res } = mockRequestResponse();
 
-    req.query = { page: "2" };
-    await handler(req, res);
+//   req.query = { page: "2" };
+//   await handler(req, res);
 
-    expect(res.statusCode).toBe(200);
-  });
+//   expect(res.statusCode).toBe(200);
+// });
 
-  // it("create a new todo", async () => {
-  //   const { req, res } = mockRequestResponse("POST");
+// it("create a new todo", async () => {
+//   const { req, res } = mockRequestResponse("POST");
 
-  //   req.body = { content: "Test todo" };
+//   req.body = { content: "Test todo" };
 
-  //   await handler(req, res);
+//   await handler(req, res);
 
-  //   expect(res.statusCode).toBe(201);
-  // });
-});
+//   expect(res.statusCode).toBe(201);
+// });
+// });
